778527955c5b55d6314e129c1e24d12371076ca6
JaiGoswami09 <jaigoswami1999@gmail.com>
2024-04-12 01:26:08 +0530

    React Installed and parcel installedWorking

--- App.js
+++ App.js
@@ -1,2 +1,27 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
+
+// Component is just a normal javascript function which return some JSX. 
+
+const Header = () => {
+     {
+        <div className="header">
+            <div className="logo-container">
+            <img className="logo" src=""></img>
+            </div>
+        </div>
+
+
+    }
+}
+
+const AppLayout = () => {
+     {
+        <div className="app"></div>
+
+    };
+};
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(); 
