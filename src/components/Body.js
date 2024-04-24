3557ee9c7d8dac95b2264a19db0f595c1d50e013
JaiGoswami09 <jaigoswami1999@gmail.com>
2024-04-24 19:24:38 +0530

    Filter Button

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,12 +3,60 @@ import RestaurantCard from "./RestaurantCard";
 import resList from "../util/mockData";
 
 const Body = () => {
+  let listofRestaurants = [
+    {
+    info: {
+      id: "65330",
+      name: "Domino's Pizza",
+      cloudinaryImageId: "d0450ce1a6ba19ea60cd724471ed54a8",
+      locality: "Old Palasia",
+      areaName: "Navneet Tower Road",
+      costForTwo: "₹400 for two",
+      cuisines: [
+        "Pizzas",
+        "Italian",
+        "Pastas",
+        "Desserts"
+      ],
+      avgRating: 3.3,
+    },
+  },
+
+  { 
+    info: {
+    id: "65859",
+    name: "Pizza Hut",
+    cloudinaryImageId: "d0450ce1a6ba19ea60cd724471ed54a8",
+    locality: "Old Palasia",
+    areaName: "Navneet Tower Road",
+    costForTwo: "₹400 for two",
+    cuisines: [
+      "Pizzas",
+      "Italian",
+      "Pastas",
+      "Desserts"
+    ],
+    avgRating: 4.5,
+
+  },
+},
+  ];
     return (
         <div className="body">
+            <div className="filter">
+                <button className="filter-btn" 
+                onClick={() => {
+                  // Filter Logic Here.
+                  listofRestaurants = listofRestaurants.filter(
+                    (res) => res.info.avgRating > 4
+                  );
+                  console.log(listofRestaurants);
+                  }}>Top-Rated Restaurant</button>
+            </div>
           //************************************************** 
             <div className="res-container">
               {
-                resList.map((restaurant) => (<RestaurantCard key={restaurant.info.id} resData = {restaurant}/>))
+                listofRestaurants.map((restaurant) => (<RestaurantCard key={restaurant.info.id} resData = {restaurant}/>))
               }
             
              </div>
