d76e6be5ffaf67afe840580152f4cd4a51cbaa1c
JaiGoswami09 <jaigoswami1999@gmail.com>
2024-07-04 23:54:55 +0530

    Updates

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 const Body = () => {
 
-  //Local State Variable:
+  //Local State Variable::
   const [listofRestaurants, setListOfRestaurant] = useState(resList);
 
   /******************************************************************
